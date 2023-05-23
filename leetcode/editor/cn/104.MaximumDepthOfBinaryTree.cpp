a15550b111f1b9de5d06c352ba9a83b2db202549
thinkbook <zhaojiangfeng1996@163.com>
2023-05-23 20:29:40 +0800

    update

--- leetcode/editor/cn/104.MaximumDepthOfBinaryTree.cpp
+++ leetcode/editor/cn/104.MaximumDepthOfBinaryTree.cpp
@@ -16,11 +16,29 @@
 //
 // Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1610 👎 0
 
- 
+
+
 #include<bits/stdc++.h>
- 
+
 using namespace std;
- 
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr)
+    {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right)
+    {}
+};
+
+
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for a binary tree node.
@@ -33,17 +51,47 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int maxDepth(TreeNode* root) {
-
+    int maxDepth(TreeNode *root)
+    {
+        if(!root)
+            return 0;
+        int level=0,thisLevelCnt=1,nextLevelCnt=0;
+        TreeNode * tmp;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            level++;
+            for(int i=0;i<thisLevelCnt;i++)
+            {
+                tmp=q.front();
+                q.pop();
+                if(tmp->left)
+                {
+                    q.push(tmp->left);
+                    nextLevelCnt++;
+                }
+                if(tmp->right)
+                {
+                    q.push(tmp->right);
+                    nextLevelCnt++;
+                }
+            }
+            thisLevelCnt=nextLevelCnt;
+            nextLevelCnt=0;
+        }
+        return level;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 
-int main(){
+int main()
+{
     Solution s;
-    
+
     return 0;
 }
